d3f5f0acf640f6c4d7bc2cdaa223dcd6f5d13312
Julia Sarris <juliasarris@gmail.com>
2024-04-15 16:27:44 -0400

    body

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,114 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  interface Entry {
+    type: string,
+    content: string
+  }
+
+  interface Commands {
+    [key: string]: {
+      desc: string,
+      content: Function
+    }
+  }
+
+  const [inputText, setInputText] = useState('');
+  const [log, setLog] = useState<Array<Entry>>([]);
+
+  const commands: Commands = {
+    'help': {
+      'desc': 'display available commands',
+      'content': () => {
+        return (
+          <div className="flex-grow flex flex-col">
+            {Object.keys(commands).map((item, idx) => (
+              <div key={idx}>
+                <span className="pl-5 text-white-400 font-bold">{item}</span>
+                <span className="text-white-400"> - {commands[item]['desc']}</span>
+              </div>
+            ))}
+          </div>
+        )
+      }
+    },
+    'echo': {
+      'desc': 'repeat text. usage: echo <text>',
+      'content': (text: string[]) => {
+        return (
+          <div className="flex-grow flex">
+            {text.join(' ')}
+          </div>
+        )
+      }
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    let args = inputText.split(' ');
+    if (event.key === 'Enter') {
+      setLog(prevLog => [...prevLog, { 'type': 'input', 'content': inputText }])
+
+      let command = args[0];
+
+      if (commands[command]) {
+        setLog(prevLog => [...prevLog, { 'type': 'output', 'content': commands[command]['content'](args.slice(1)) } ])
+      }
+      setInputText('');
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-r from-violet-500 to-fuchsia-500 ">
+
+      <div className="w-full">
+        <div className="coding inverse-toggle px-5 pt-4 shadow-lg text-gray-100 text-sm font-mono subpixel-antialiased 
+              bg-gray-800  pb-6 pt-4 rounded-lg leading-normal overflow-hidden">
+          <div className="top mb-2 flex">
+            <div className="h-3 w-3 bg-red-500 rounded-full"></div>
+            <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
+            <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
+          </div>
+          <div className="mt-4 flex flex-col">
+
+            <div className="flex-grow flex">
+              <span className="text-green-400">computer:~$</span>
+              <p className="flex-1 typing items-center pl-2">
+                help
+              </p>
+              <br/>
+            </div>
+
+            <div className="flex-grow flex">
+              {commands['help']['content']()}
+            </div>
+
+            <div id="log" className="flex-grow flex flex-col">
+              {log.map((item, idx) => (
+                <div key={idx} className="flex-grow flex">
+                  { item['type'] == 'input' && <span className="text-green-400">computer:~$</span> }
+                  <p className="flex-1 typing items-center pl-2">{item['content']}</p>
+                </div>
+              ))}
+            </div>
+
+            <div id="prompt" className="flex-grow flex">
+              <span className="text-green-400">computer:~$</span>
+              <p className="flex-1 typing items-center pl-2">
+                <input id="terminal-input" className="text-white-400 text-white bg-transparent w-full outline-none" 
+                  value={inputText} autoFocus onKeyDown={handleKeyDown} onChange={(e) => setInputText(e.target.value)}></input>
+                <br></br>
+              </p>
+            </div>
+  
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+   
     </main>
   );
 }
