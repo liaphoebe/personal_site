e550fe689c4ff5b165db233c64dbf5b6849e4259
Julia Sarris <juliasarris@gmail.com>
2024-04-16 09:49:54 -0400

    label test

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       </head>
       <body>
       <div className="bg-gradient-to-r from-purple-500 to-fuchsia-500 p-3 sticky top-0 shadow-md">
